022a70be5a16f0da5a6174a65c5ff3df8b40a788
christianbenson <Christianmbenson@gmail.com>
2023-03-02 10:43:11 -0600

    commit message

---
+++ HelloMessages.cs
@@ -0,0 +1,26 @@
+namespace HelloMethods
+{
+    public class Message
+    {
+        public static string GetMessage(string lang)
+        {
+            if (lang.Equals("sp")){
+                return "Hola Mundo";
+            }
+            else if (lang.Equals("fr"))
+            {
+                return "Bonjour le monde";
+            }
+            else if (lang.Equals("nl"))
+            {
+                return "Hallo wereled";
+            }
+            else
+            {
+                return "hello world";
+            }
+        
+            
+        }
+    }
+}
